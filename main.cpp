c2cdc2afc0c2020784745cb4cf78067e1532d6e2
TheRealJKells <jkeller1@carthage.edu>
2019-02-20 15:08:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
+#include <math>
 
 using namespace std;
 
 int main()
 {
+    square(10);
 
     return 0;
+}
+
+void square(int number)
+{
+    number = number*number;
+
+    cout << number;
 }
